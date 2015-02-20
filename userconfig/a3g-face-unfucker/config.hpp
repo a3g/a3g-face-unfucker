8ab086bd6bdd3e435d23a4af9d566d1fc2346b59
Cephei <cepheidev+github@gmail.com>
2015-02-21 00:38:52 +0100

    Some formatting fix

--- userconfig/a3g-face-unfucker/config.hpp
+++ userconfig/a3g-face-unfucker/config.hpp
@@ -1,13 +1,13 @@
 class Config {
   whiteList[] = {};
   blackList[] = {};
-  useBlacklistInstead = "false";        // If you set this to true, the blacklist is enabled instead of whitelist.
+  useBlacklistInstead = "false";            // If you set this to true, the blacklist is enabled instead of whitelist.
   
   // Example:
   // whiteList[] = {"76561197961167880"};   // Id's have to be seperated with a comma AND surrounded by quotes.
-  // blackList[] = {};            // Two Id's would look like this: whiteList[] = {"76561197961167880", "76561197961167880"};
-                        // An empty list has no effect.
+  // blackList[] = {};                      // Two Id's would look like this: whiteList[] = {"76561197961167880", "76561197961167880"};
+                                            // An empty list has no effect.
   
-  // These need to be the unique player Id's. You can find them by looking up their Steam64 Id, or player Id ( you can find them in their Arma 3 profile ).
-  // The above setting is comfy blanket btw :^).
+                                            // These need to be the unique player Id's. You can find them by looking up their Steam64 Id, or player Id ( you can find them in their Arma 3 profile ).
+                                            // The above setting is comfy blanket btw :^).
 };
